9c52be4666686d3f33d6cbd0a870407eecc319b4
lcedrez <102386169+lcedrez@users.noreply.github.com>
2022-05-15 20:47:37 -0300

    juego del 1 al 10

---
+++ JavaScript/app.js
@@ -0,0 +1,50 @@
+
+
+var numero=0
+var contador =0
+var intentos=5
+
+alert('Adivina el numero del 1 al 10 en 5 intentos')
+while(numero!=3)
+{
+    
+    numero=prompt("Ingrese Número:")
+    if(isNaN(numero))
+    {
+        alert("Debe Ingresar un Numero no se permiten Textos")
+    }
+   else if(numero <=0 || numero >= 10)
+    {
+        alert("Debe Ingresar un Número entre 1 y 10")
+       
+    }
+    else{
+
+    if(numero==3)
+    {
+        alert("Felicitaciones adivinaste el numero!!")
+    }
+    else
+    {
+        
+            contador++
+            cantidad=5-contador
+            
+            alert("Fallaste te quedan" +' '+ cantidad + ' '+  'Intentos')
+
+    }
+    if(contador==5)
+    {
+        alert("Lo siento has perdido el Juego!!")
+    }
+    
+}
+       
+    
+
+   
+}
+
+   
+   
+
